8fb88d946fafa0cef1dea23eadd48b0b492e70fb
Alex Davis <adavis1152@gmail.com>
2021-06-03 14:45:48 -0600

    upload of basic map code and notes for cleaning

---
+++ Code/Notes on data cleaning.R
@@ -0,0 +1,13 @@
+### Notes on data cleaning needed before we can create the map###
+#1. import csvs, 3 total
+#2. left merge the csvs together?
+  # Species <- Transects <- Site_metadata
+  # merge the transect info to the species using transect id
+  # merge the site meta data using Site name
+#3. amalgamate the the fish into abundance by species and and site, should
+  # end up with two numbers per site for a total of 86 rows of data
+#4. calculate the area of each site based on length and width
+#5. calculate density of the two species for each site
+#7. map the species and color by density, shape for species ie two shapes/site
+#-- spit and map only one species at at time? 
+#-- have two maps side by side?
